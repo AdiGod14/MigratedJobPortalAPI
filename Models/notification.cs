7ba6cdc990b753db983bd71bbb2e4ead962ec966
AdiGod14 <aditya.gode@gmail.com>
2025-07-18 15:10:26 +0530

    models migrated

--- Models/notification.cs
+++ Models/notification.cs
@@ -1,6 +1,27 @@
-﻿namespace MigratedJobPortalAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MigratedJobPortalAPI.Models
 {
-    public class notification
+    public class Notification
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("userId")]
+        [BsonRequired]
+        public string UserId { get; set; }
+
+        [BsonElement("message")]
+        [BsonRequired]
+        public string Message { get; set; }
+
+        [BsonElement("isRead")]
+        public bool IsRead { get; set; } = false;
+
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
